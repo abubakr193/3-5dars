5b63887c3c6f0c7eebf372248fb89e2c96089da9
abubakr193 <salimboy19941409@gmail.com>
2022-02-21 23:09:15 +0500

    survey

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// Survey
+var survey = alert("Assalomu alaykum, iltimos so'rovnomani to'ldiring");
+
+// User info
+var userName = prompt("ismingizni yozing...");
+
+var userSurname = prompt("familiyangizni yozing...");
+
+var userAge = prompt("yoshingizni yozing...");
+
+var userAccommodation = prompt("qayshi shaharda yashashingizni yozing...");
+
+var isStudent = prompt("talabamisiz...");
+
+var telNumber = prompt("telefon raqamingizni kiriting", 998);
+
+var isMarried = prompt("oila qurganmisiz...");
+
+// Survey result
+var result = alert("Ismingiz: " + userName + "\n"  + "Familiyangiz: " +  userSurname + "\n" + "Yoshingiz: " +  userAge + "\n" + "Yashash shaharingiz: " +  userAccommodation + "\n"  + "Talabamisiz: " + isStudent + "\n" + "Telefon raqamingiz: " +  telNumber + "\n" + "Oilalimisiz: " + isMarried);
